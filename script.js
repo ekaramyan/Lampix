ceb5b8e274f3fbed342dd9116f84af2222a77ff5
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2022-10-11 03:45:27 +0400

    adaptated for horizontal orietation

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ $(document).ready(function(){
     $('.burger-menu__button').click(function(event){
     $('.burger-menu, .menu, burger-menu__button-container').toggleClass('active');
 
-    $('.search').click(function(event)
-    {
+
+});
+
+$(document).ready(function(){
+    $('.search-wrap').click(function(event){
         $('.searchTerm').toggleClass('active');
     });
     });
