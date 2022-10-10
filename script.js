1de413e2a6251db38ea07adf0c92088ba0b6c33b
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2022-10-11 02:37:09 +0400

    adaptated footer fo phones

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
     $('.burger-menu__button').click(function(event){
     $('.burger-menu, .menu, burger-menu__button-container').toggleClass('active');
+
+    $('.search').click(function(event)
+    {
+        $('.searchTerm').toggleClass('active');
+    });
     });
 });
+
+
+//js
+// function ActiveClass() {
+//     var element = document.getElementById("burger-menu__btn");
+//     element.classList.toggle(".burger-menu.active", ".menu.active");
+//   }
