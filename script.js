8b9d9aa483df8f2bc19c809b419ad3bf7c9842c2
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2022-10-28 22:32:38 +0400

    making scripts for site

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ for (i = 0; i < acc.length; i++) {
         /* Toggle between adding and removing the "active" class,
         to highlight the button that controls the panel */
         this.classList.toggle("active");
-
+        
         /* Toggle between hiding and showing the active panel */
         var panel = this.nextElementSibling;
         if (panel.style.display === "block") {
