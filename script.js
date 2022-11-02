8f6315abe6e44427d9d60ece8fd20ef5b2792e79
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2022-11-02 22:56:43 +0400

    adding new scripts

--- script.js
+++ script.js
@@ -49,25 +49,30 @@ if (menuClose) {
 let acc = document.getElementsByClassName("accordion");
 let i;
 
+acc.onclick=function(){
+    acc.classList.toggle('active')
+}
+
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
         /* Toggle between adding and removing the "active" class,
         to highlight the button that controls the panel */
         this.classList.toggle("active");
 
+
         /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
+        const panel = this.nextElementSibling;
         if (panel.style.display === "block") {
             panel.style.display = "none";
         } else {
-            panel.style.display = "block";
+            panel.style.display = "block";      
         }
     });
 }
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
         this.classList.toggle("active");
-        var panel = this.nextElementSibling;
+        const panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
         } else {
@@ -82,20 +87,24 @@ for (i = 0; i < acc.length; i++) {
 //   }
 
 
-// accordion:after.onclick = function(){
-//     this.classList.toggle('rotate')
-//   }
-
 function rangeFunction(){
 let rng = document.getElementById('myRange'); //rng - это ползунок
 let out = document.getElementById('range_count'); // i1 - input
 let coins = document.getElementById('range_coins');
+const min = rng.min
+const max = rng.max
+const value = rng.value
+rng.style.background = `linear-gradient(to right, #FFD699 0%, #FFD699 ${(value-min)/(max-min)*100}%, #DEE2E6 ${(value-min)/(max-min)*100}%, #DEE2E6 100%)`
+
 out.innerHTML = rng.value;
 coins.innerHTML = rng.value;
+
 rng.oninput = function () {
     out.innerHTML = parseInt(this.value * 10);
     coins.innerHTML = parseInt(this.value * 14);
-}
-}
+    //#FFD699
+    this.style.background = `linear-gradient(to right, #FFD699 0%, #FFD699 ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 100%)`
 
+}
+}
 rangeFunction();
