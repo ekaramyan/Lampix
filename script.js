5af78a5d7d6c144d64b374a9dab302162e6b9463
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2022-11-05 17:51:28 +0400

    starting writing new functions

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-// $(document).ready(function () {
-//     $('.burger-menu__button').click(function (event) {
-//         $('.burger-menu, .menu, .burger-menu__button-container .burger-menu__content').toggleClass('active');
-//     });
-
 $(document).ready(function () {
     $('.search-wrap').click(function (event) {
         $('.searchTerm').toggleClass('active');
     });
 });
-// });
+
+const search = function () {
+
+}
 
 const menu_btn = document.getElementById('burger-menu__btn');
 if (menu_btn) {
-    // const menu_button = document.querySelector('.burger-menu__btn');
     const burger_menu = document.getElementById('burger-menu');
     const burger_menu_content = document.getElementById('menu');
     menu_btn.addEventListener('click', function (e) {
-        // menu_button.classList.toggle('active');
         burger_menu.classList.add('active');
         burger_menu_content.classList.add('active');
     });
@@ -32,29 +28,15 @@ if (menuClose) {
     });
 }
 
-// const box = document.querySelector('.burger-menu');
-
-// document.addEventListener('click', (e) => {
-//     const click = e.composedPath().includes(box);
-//     if (!click) {
-//         // box.style.display = 'none';
-//         // box.classList.toggle('active')
-//     }
-// })
-
-
-
-
-
 let acc = document.getElementsByClassName("accordion");
 let acc_arr = document.getElementsByClassName("accordion__arrow");
 let i;
 
-acc.onclick=function(){
+acc.onclick = function () {
     acc.classList.toggle('active')
 }
 
-acc_arr.onclick=function(){
+acc_arr.onclick = function () {
     acc_arr.classList.toggle('active')
 }
 
@@ -70,7 +52,7 @@ for (i = 0; i < acc.length; i++) {
         if (panel.style.display === "block") {
             panel.style.display = "none";
         } else {
-            panel.style.display = "block";      
+            panel.style.display = "block";
         }
     });
 }
@@ -85,31 +67,24 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
-//js
-// function ActiveClass() {
-//     var element = document.getElementById(".burger-menu__btn, .burger-menu__button");
-//     element.classList.toggle(".burger-menu.active", ".menu.active");
-//   }
 
+function rangeFunction() {
+    let rng = document.getElementById('myRange'); //rng - это ползунок
+    let out = document.getElementById('range_count'); // i1 - input
+    let coins = document.getElementById('range_coins');
+    const min = rng.min
+    const max = rng.max
+    const value = rng.value
+    rng.style.background = `linear-gradient(to right, #FFD699 0%, #FFD699 ${(value - min) / (max - min) * 100}%, #DEE2E6 ${(value - min) / (max - min) * 100}%, #DEE2E6 100%)`
 
-function rangeFunction(){
-let rng = document.getElementById('myRange'); //rng - это ползунок
-let out = document.getElementById('range_count'); // i1 - input
-let coins = document.getElementById('range_coins');
-const min = rng.min
-const max = rng.max
-const value = rng.value
-rng.style.background = `linear-gradient(to right, #FFD699 0%, #FFD699 ${(value-min)/(max-min)*100}%, #DEE2E6 ${(value-min)/(max-min)*100}%, #DEE2E6 100%)`
+    out.innerHTML = rng.value;
+    coins.innerHTML = rng.value;
 
-out.innerHTML = rng.value;
-coins.innerHTML = rng.value;
+    rng.oninput = function () {
+        out.innerHTML = parseInt(this.value * 10);
+        coins.innerHTML = parseInt(this.value * 14);
+        this.style.background = `linear-gradient(to right, #FFD699 0%, #FFD699 ${(this.value - this.min) / (this.max - this.min) * 100}%, #DEE2E6 ${(this.value - this.min) / (this.max - this.min) * 100}%, #DEE2E6 100%)`
 
-rng.oninput = function () {
-    out.innerHTML = parseInt(this.value * 10);
-    coins.innerHTML = parseInt(this.value * 14);
-    //#FFD699
-    this.style.background = `linear-gradient(to right, #FFD699 0%, #FFD699 ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 ${(this.value-this.min)/(this.max-this.min)*100}%, #DEE2E6 100%)`
-
-}
+    }
 }
 rangeFunction();
