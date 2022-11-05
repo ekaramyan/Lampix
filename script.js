93f30079c439b452fc14a9c115c8b6eecb364b9e
Edos Ddos <56231244+ekaramyan@users.noreply.github.com>
2022-11-05 21:41:21 +0400

    trying to rotate arrow by click

--- script.js
+++ script.js
@@ -34,19 +34,15 @@ let i;
 
 acc.onclick = function () {
     acc.classList.toggle('active')
-}
-
-acc_arr.onclick = function () {
     acc_arr.classList.toggle('active')
 }
+
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
         /* Toggle between adding and removing the "active" class,
         to highlight the button that controls the panel */
         this.classList.toggle("active");
-
-
         /* Toggle between hiding and showing the active panel */
         const panel = this.nextElementSibling;
         if (panel.style.display === "block") {
